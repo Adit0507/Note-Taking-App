ecb5849e2f8c9e26124286c37268fd69a7e10aaf
Adit0507 <adi05072002@gmail.com>
2023-03-11 15:40:11 +0530

    New note done

---
+++ src/NewNote.tsx
@@ -0,0 +1,22 @@
+import { NoteForm } from "./NoteForm";
+import { NoteData, Tag } from "./App";
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
+  return 
+  <>
+    <h1 className="mb-4">New Note</h1>  
+    <NoteForm
+      onSubmit={onSubmit}
+      onAddTag={onAddTag}
+      availableTags={availableTags}
+    />
+  </>;
+}
+
+export default NewNote;
