aad5a9f79d84a10df2e72d0596459c479ccd6404
Adit0507 <adi05072002@gmail.com>
2023-03-26 15:10:23 +0530

    Fixed the export statement

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -8,15 +8,14 @@ type NewNoteProps = {
 };
 
 export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
-  return 
-  <>
-    <h1 className="mb-4">New Note</h1>  
-    <NoteForm
-      onSubmit={onSubmit}
-      onAddTag={onAddTag}
-      availableTags={availableTags}
-    />
-  </>;
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  );
 }
-
-export default NewNote;
